eac8c1bcf3487db91c238d810ce2ec545ca4d15f
Ahmed Rashad <supermody1997@hotmail.com>
2023-04-25 14:29:58 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,10 @@ export default {
   content: [
     "./index.html",
 	"./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tw-elements/dist/plugin.cjs")]
+  plugins: []
 }
 
